9bcf81e748b0aa1ddc8730d8bb08f8051b457d69
Vivek1o1 <79412190+Vivek1o1@users.noreply.github.com>
2021-06-07 17:51:39 +0530

    Create first_repeating_element.cpp

---
+++ first_repeating_element.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+   
+   int n;
+   cin>>n;
+   int arr[n];
+   for(int i=0;i<n;i++){
+       cin>>arr[i];
+   }
+   for(int i=0;i<n-1;i++){
+       for(int j=i+1;j<n;j++){
+           if(arr[i]==arr[j]){
+               cout<<"first repeating element is : "<<arr[i];
+               return 0;
+           }
+     
+          
+   }
+  
+   }
+   cout<<"no repeating elements";
+               return 0;
+   
+}
