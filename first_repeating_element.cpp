751ade35cef39a14bdf27913d3fd9862bd84251f
Vivek1o1 <79412190+Vivek1o1@users.noreply.github.com>
2021-06-09 00:12:35 +0530

    Update first_repeating_element.cpp

--- first_repeating_element.cpp
+++ first_repeating_element.cpp
@@ -1,3 +1,32 @@
+// o(n)
+#include <iostream>
+#include <limits.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+    int arr[n];
+    for(int i=0;i<n;i++){
+        cin>> arr[i];
+    }
+    int N = 1e6+2;
+    int check[N];
+    for(int i=0;i<N;i++){
+        check[i] = 0;
+    }
+    for(int i=0;i<n;i++){
+        if(check[arr[i]]!=0){
+            cout<<arr[i];
+            return 0;
+        } else {
+            check[arr[i]] += 1;
+        }
+    }
+    cout<<-1;
+}
+// o(n^2)
 #include <iostream>
 using namespace std;
 
