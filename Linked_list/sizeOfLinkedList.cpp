20b1a9a41f30055c5cec13a83871f9212ebda489
Vivek Singh <79412190+Vivek1o1@users.noreply.github.com>
2021-08-10 19:08:42 +0530

    Create sizeOfLinkedList.cpp

---
+++ Linked_list/sizeOfLinkedList.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Node{
+    public:
+  int data;
+  Node* next;
+
+  Node(int val){
+      data=val;
+      next=NULL;
+  }
+};
+
+int sizeOfLinkedList(Node* head){
+    int counter=0;
+    Node* temp= head;
+    if(head==NULL){
+        return 0;
+    }
+    while(temp!=NULL){
+        counter++;
+        temp=temp->next;
+    }
+    return counter;
+}
+
+void insertAtTail(Node* &head,int val){
+    Node* n= new Node(val);
+    Node* temp= head;
+    if(head==NULL){
+        head=n;
+        return;
+    }
+    if(temp->next!=NULL){
+        temp=temp->next;
+    }
+    temp->next=n;
+}
+
+void displayNode(Node* head){
+    Node* temp= head;
+   while(temp!=NULL){
+       cout<<temp->data<<"->";
+       temp=temp->next;
+   }
+   cout<<"NULL"<<endl;
+}
+int main(){
+   Node* head= NULL;
+   insertAtTail(head,1);
+   insertAtTail(head,2);
+   insertAtTail(head,3);
+   cout<<sizeOfLinkedList(head)<<endl;
+   displayNode(head);
+
+   
+}
